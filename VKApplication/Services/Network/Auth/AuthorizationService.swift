1a25964e016a51cc1f7679457cce96fadb120487
Natalia Volkova <nataliii.volkova@gmail.com>
2017-10-29 22:48:16 +0300

    Изменен параметр запрашиваемых разрешений для запроса

--- VKApplication/Services/Network/Auth/AuthorizationService.swift
+++ VKApplication/Services/Network/Auth/AuthorizationService.swift
@@ -21,7 +21,7 @@ class AuthorizationService {
             URLQueryItem(name: "client_id", value: "6221547"),
             URLQueryItem(name: "display", value: "mobile"),
             URLQueryItem(name: "redirect_uri", value: "https://oauth.vk.com/blank.html"),
-            URLQueryItem(name: "scope", value: "327686"),
+            URLQueryItem(name: "scope", value: "1515550"),
             URLQueryItem(name: "response_type", value: "token"),
             URLQueryItem(name: "v", value: "5.68")
         ]
