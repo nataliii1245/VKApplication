0321065e438cbe03e1355a15e205856bc16a5c12
Natalia Volkova <nataliii.volkova@gmail.com>
2017-11-08 09:29:56 +0300

    Добавлен класс-отладчик

--- VKApplication/AppDelegate.swift
+++ VKApplication/AppDelegate.swift
@@ -7,15 +7,28 @@
 //
 
 import UIKit
+import SwiftyJSON
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         return true
+    }
+    
+}
+
+class Bug {
+    
+    static var shared = Bug()
+    
+    private init() { }
+    
+    func `catch`(_ json: JSON) {
+        print("bug")
+        print(json)
     }
     
 }
